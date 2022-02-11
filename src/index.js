f08f93fcc71af61d9fe4cea6796a5df28b2a7427
Jacob Lee <jacob@bitski.com>
2022-02-11 12:09:56 -0800

    Add support for sccache

--- src/index.js
+++ src/index.js
@@ -19,13 +19,12 @@ async function withEnv(fn) {
   return result;
 }
 
-async function withCacheArgs(fn) {
+async function withCacheArgs(fn, useSccache) {
   return await withEnv(async () => {
     const PATH = core.getMultilineInput("path");
     const TAG = core.getInput("tag");
 
     const CARGO_HOME = process.env.CARGO_HOME ?? "~/.cargo";
-    const CARGO_TARGET_DIR = process.env.CARGO_TARGET_DIR ?? "target";
     const RUNNER_OS = process.env.RUNNER_OS;
     const RUNNER_ARCH = process.env.RUNNER_ARCH;
 
@@ -34,22 +33,33 @@ async function withCacheArgs(fn) {
       ["rust-toolchain.toml", "**/Cargo.lock"].join("\n")
     );
 
+    const inputs = [
+      `${CARGO_HOME}/bin`,
+      `${CARGO_HOME}/registry/cache`,
+      `${CARGO_HOME}/registry/index`,
+      `${CARGO_HOME}/git/db`,
+    ];
+
+    if (!useSccache) {
+      const CARGO_TARGET_DIR =
+        process.env.CARGO_TARGET_DIR ??
+        path.join(process.env.GITHUB_WORKSPACE, "target");
+      inputs.push(CARGO_TARGET_DIR);
+    }
+
     process.env.INPUT_PATH = PATH.concat(
-      [
-        `${CARGO_HOME}/bin`,
-        `${CARGO_HOME}/registry/cache`,
-        `${CARGO_HOME}/registry/index`,
-        `${CARGO_HOME}/git/db`,
-        `${CARGO_TARGET_DIR}`,
-      ].filter((path) => PATH.indexOf(path) < 0)
+      inputs.filter((path) => PATH.indexOf(path) < 0)
     ).join("\n");
+    core.info(`path=${process.env.INPUT_PATH}`);
 
     process.env.INPUT_KEY = `${PREFIX}-${HASH}-${TAG}`;
+    core.info(`key=${process.env.INPUT_KEY}`);
 
     process.env["INPUT_RESTORE-KEYS"] = [
       `${PREFIX}-${HASH}-`,
       `${PREFIX}-`,
     ].join("\n");
+    core.info(`restore-keys=${process.env["INPUT_RESTORE-KEYS"]}`);
 
     return await Promise.resolve(fn());
   });
@@ -65,22 +75,40 @@ function isCacheHit() {
   );
 }
 
+async function isSccacheConfigured() {
+  return (
+    process.env.RUSTC_WRAPPER?.indexOf("sccache") >= 0 &&
+    (await io.which("sccache"))
+  );
+}
+
 module.exports.run = async function run() {
-  await core.group("Run actions/cache@v2", async () => {
+  const useSccache = isSccacheConfigured();
+
+  await core.group("sccache", async () => {
+    if (useSccache) {
+      core.info("Starting sccache server");
+      await exec.exec("sccache", ["--show-stats"]);
+    } else {
+      core.info("sccache is not configured");
+    }
+  });
+
+  await core.group("actions/cache@v2", async () => {
     await withCacheArgs(async () => {
       await restore();
-    });
+    }, useSccache);
   });
 };
 
 module.exports.runPost = async function runPost() {
   await core.group("cargo-cache", async () => {
     if (isCacheHit()) {
-      core.info("Cache hit, not cleaning cache.");
+      core.info("Cache hit, not cleaning cache");
       return;
     }
 
-    if (!(await io.which("cargo-cache", false))) {
+    if (!(await io.which("cargo-cache"))) {
       await exec.exec("cargo", [
         "install",
         "--no-default-features",
@@ -91,6 +119,15 @@ module.exports.runPost = async function runPost() {
     }
 
     await exec.exec("cargo-cache", ["--autoclean"]);
+  });
+
+  await core.group("sccache", async () => {
+    if (isSccacheConfigured()) {
+      core.info("Stopping sccache server");
+      await exec.exec("sccache", ["--stop-server"]);
+    } else {
+      core.info("sccache is not configured");
+    }
   });
 
   await core.group("actions/cache@v2", async () => {
